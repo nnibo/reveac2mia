a5daff65ab341419bebc48572805e75369099f0d
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-01 12:03:17 -0300

    Ac finalizada

--- reveac2mia.js
+++ reveac2mia.js
@@ -1,67 +1,82 @@
-// instalando programas
-const express = require("express");
+//instalando programas
 const mongoose = require("mongoose");
+const express = require("express");
 const bodyParser = require("body-parser");
-const res = require("express/lib/response");
 
-// configurando o roteamento para teste no postman
+//configurando o roteamento para teste no postman
 const app = express();
-app.use((bodyParser.urlencoded({extend: true})));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 const port = 3000;
 
 //configurando o acesso ao mongodb
-mongoose.connect('mongodb://127.0.0.1:27017/reveac2mia',
-{
+mongoose.connect('mongodb://127.0.0.1:27017/teste',{
     useNewUrlParser : true,
-    useUnifiedTopology: true,
-    // serverSelectionTimoutMS: 20000
+    useUnifiedTopology : true
+    //serverSelectionTimeoutMS : 20000
 })
 
-//criando a model do seu projeto
+//criando a model do seu projeto, baseando se nos seus atributos
 const PessoaSchema = new mongoose.Schema({
     nome : {type : String},
-    email : {type: String, required: true},
-    endereco : { type : String},
-    numero : {type: Number},
-    cep : {type : String, required: true},
-    nasicmento : {type : Date, required: true}
-})
+    email : {type : String, require : true},
+    endereco : {type : String},
+    numero : {type : Number},
+    cep : {type : String, require : true},
+    nascimento : {type : Date, require : true}
+}) 
 
+const Pessoa = mongoose.model("Pessoa", PessoaSchema);
 
-const Pessoa = new mongoose.model("PEssoa",PessoaSchema);
+//configurando os roteamentos 
+app.post("/cadastropessoa", async(req, res)=>{
+    const nome = req.body.nome;
+    const email = req.body.email;
+    const endereco = req.body.endereco;
+    const numero = req.body.numero;
+    const cep = req.body.cep;
+    const nascimento = req.body.nascimento;
 
-//configurando os roteamentos
-app.post("/cadastropessoa",async(req, res) => {
-    const nome = req.body.nome
-    const email = req.body.email
-    const endereco = req.body.endereco
-    const numero = req.body.numero
-    const cep = req.body.cep
-    const nascimento = req.body.nascimento
+    if (nome == null || email == null || endereco == null || numero == null || cep == null || 
+        nascimento == null ){
+        return res.status(400).json({error : "Preencher todos os campos"});
+        }
 
-    const Pessoa = new Pessoa({
-        nome : "Nibaum",
-        email : "nibaum@gmail.com",
-        endereco: "Rua Nibaum 123",
-        numero : 12345-1234,
-        cep : "12345-060",
-        nasicmento : "03/02/07"
+// teste mais importante da AC
+    const emailExiste = await Pessoa.findOne({email : email});
+
+    if(emailExiste){
+        return res.status(400).json({error : "O email cadastrado já existe!"})
+    }
+
+    const pessoa = new Pessoa({
+        nome : nome,
+        email : email,
+        endereco : endereco,
+        numero : numero,
+        cep : cep,
+        nascimento : nascimento,
     })
 
     try{
-        const newPessoa = await Pessoa.save()
-        res.json({error : null, msg: "Cadastro ok", pessoaId : newPessoa_id});
-    } catch(error){
-        res.status(400).json({error})
+        const newPessoa = await pessoa.save();
+        res.json({error : null, msg : "Cadastro ok", pessoaId : newPessoa._id});
+    } 
+    catch(error){
+        res.status(400).json((error))
     }
-});
-
-app.get("/", async(req,res)=>{
-    res.sendFile(__dirname +"/index.html")
+})
+//rota para get de cadastro
+app.get("/cadastropessoa", async (req,res)=>{
+    res.sendFile(__dirname + "/cadastropessoa.html")
 })
 
-//configurando a porta
+//rota raiz -inw
+app.get("/", async (req,res)=>{
+    res.sendFile(__dirname + "/index.html")
+})
+
+//configurando a porta 
 app.listen(port, ()=>{
-    console.log(`Servidor rodando na porta ${port}`)})
-    
+    console.log(`Servidor rodando na porta ${port}`)
+});
